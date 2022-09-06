4ad697d2b37def08aec37e6592e2381a12f463f3
Galen Yang <galen.yang@vista.co>
2022-09-06 21:31:35 +1200

    Specflow Implementation

---
+++ August2022/August2022/StepDefinitions/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace August2022.StepDefinitions
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given(@"I logged into turn up portal successfully")]
+        public void GivenILoggedIntoTurnUpPortalSuccessfully()
+        {
+
+        }
+
+        [When(@"I navigate to time and material page")]
+        public void WhenINavigateToTimeAndMaterialPage()
+        {
+
+        }
+
+        [When(@"I create a new time and material record")]
+        public void WhenICreateANewTimeAndMaterialRecord()
+        {
+
+        }
+
+        [Then(@"The record should be create successfully")]
+        public void ThenTheRecordShouldBeCreateSuccessfully()
+        {
+
+        }
+    }
+}
